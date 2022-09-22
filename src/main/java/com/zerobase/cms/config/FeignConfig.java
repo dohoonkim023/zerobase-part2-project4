a8d8434964c136f0a7efb7b2ad0d99bdbd21a1e4
dohoonkim <dohoonkim023@gmail.com>
2022-09-22 15:35:10 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ src/main/java/com/zerobase/cms/config/FeignConfig.java
@@ -0,0 +1,16 @@
+package com.zerobase.cms.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("api", "2ea966c0a273c78d412db5668db995a9-78651cec-4d75b647");
+    }
+}
