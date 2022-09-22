beb9757f8d3c4353da4df177a33cbed48d74be16
dohoonkim <dohoonkim023@gmail.com>
2022-09-22 15:15:48 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ src/main/java/com/zerobase/cms/domain/model/Customer.java
@@ -0,0 +1,48 @@
+package com.zerobase.cms.domain.model;
+
+import com.zerobase.cms.domain.SignUpForm;
+import lombok.*;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class)
+public class Customer extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String password;
+    private String phone;
+    private LocalDateTime birth;
+
+    private LocalDateTime verifyExpiredAt;
+    private String verificationCode;
+    private boolean verify = false;
+
+    public static Customer from(SignUpForm form) {
+        return Customer.builder()
+                .email(form.getEmail().toLowerCase(Locale.ROOT))
+                .password(form.getPassword())
+                .name(form.getName())
+                .birth(form.getBirth())
+                .phone(form.getPhone())
+                .verify(false)
+                .build();
+    }
+
+
+
+}
---
+++ src/test/java/com/zerobase/cms/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,32 @@
+package com.zerobase.cms.service;
+
+import com.zerobase.cms.domain.SignUpForm;
+import com.zerobase.cms.domain.model.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService signUpCustomerService;
+
+    @Test
+    void signUp() {
+        SignUpForm form = SignUpForm.builder()
+                .name("name")
+                .birth(LocalDateTime.now())
+                .email("abc@gmail.com")
+                .password("1")
+                .phone("010000000")
+                .build();
+        Customer customer = signUpCustomerService.signUp(form);
+        Assert.isTrue(signUpCustomerService.signUp(form).getId()!= null);
+    }
+}
